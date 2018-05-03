9d43fe85170b6797fee8bce0427e21421a72f495
Shaun Peet <shaunpeet@microagebasics.com>
2018-05-03 15:00:33 -0400

    Updated both projects to add Identty templates (was suppsoed to happen when the project was created - oops)

--- aspnetcorebook/ShaunToDoProject/Controllers/HomeController.cs
+++ aspnetcorebook/ShaunToDoProject/Controllers/HomeController.cs
@@ -8,30 +8,30 @@ using ShaunToDoProject.Models;
 
 namespace ShaunToDoProject.Controllers
 {
-  public class HomeController : Controller
-  {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-
-    public IActionResult About()
-    {
-      ViewData["Message"] = "Your application description page.";
-
-      return View();
-    }
-
-    public IActionResult Contact()
-    {
-      ViewData["Message"] = "Your contact page.";
-
-      return View();
-    }
-
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-  }
 }
--- aspnetcorebook/ShaunToDoProject/Startup.cs
+++ aspnetcorebook/ShaunToDoProject/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ShaunToDoProject.Data;
+using ShaunToDoProject.Models;
 using ShaunToDoProject.Services;
 
 namespace ShaunToDoProject
@@ -22,6 +26,16 @@ namespace ShaunToDoProject
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+
+      services.AddDbContext<ApplicationDbContext>(options =>
+          options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+          .AddEntityFrameworkStores<ApplicationDbContext>()
+          .AddDefaultTokenProviders();
+
+      // Add application services.
+      services.AddTransient<IEmailSender, EmailSender>();
 
       services.AddSingleton<ITodoItemService, FakeTodoItemService>();
 
