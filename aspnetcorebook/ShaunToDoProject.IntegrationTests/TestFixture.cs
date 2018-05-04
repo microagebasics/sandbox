22890cc94334bdfb7b86c7e9fca60b37d50298f1
Shaun Peet <shaunpeet@microagebasics.com>
2018-05-04 12:03:20 -0400

    Integration Tests complete.  Through page 110

---
+++ aspnetcorebook/ShaunToDoProject.IntegrationTests/TestFixture.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace ShaunToDoProject.IntegrationTests
+{
+  public class TestFixture : IDisposable
+  {
+
+    private readonly TestServer _server;
+
+    public TestFixture()
+    {
+      var builder = new WebHostBuilder()
+        .UseStartup<ShaunToDoProject.Startup>()
+        .ConfigureAppConfiguration((context, configBuilder) =>
+        {
+          configBuilder.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\..\\ShaunToDoProject"));
+          configBuilder.AddJsonFile("appsettings.json");
+          // don't need the Facebook stuff as we didn't connect to it
+        });
+
+      _server = new TestServer(builder);
+
+      Client = _server.CreateClient();
+      Client.BaseAddress = new Uri("http://localhost:5001");
+    }
+
+    public HttpClient Client { get; }
+
+    public void Dispose()
+    {
+      Client.Dispose();
+      _server.Dispose();
+    }
+  }
+}
---
+++ aspnetcorebook/ShaunToDoProject.IntegrationTests/TodoRouteShould.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShaunToDoProject.IntegrationTests
+{
+  public class TodoRouteShould : IClassFixture<TestFixture>
+  {
+
+    private readonly HttpClient _client;
+
+    public TodoRouteShould(TestFixture fixture)
+    {
+      _client = fixture.Client;
+    }
+
+    [Fact]
+    public async Task ChallengeAnonymousUser()
+    {
+      // Arrange
+      var request = new HttpRequestMessage(HttpMethod.Get, "/todo");
+
+      // Act
+      var response = await _client.SendAsync(request);
+
+      // Assert
+      Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+      Assert.Equal("http://localhost:5001/Account/Login?ReturnUrl=%2Ftodo", response.Headers.Location.ToString());
+
+    }
+  }
+}
