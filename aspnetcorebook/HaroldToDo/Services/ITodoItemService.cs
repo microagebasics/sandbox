c83a562284c506fd52531dfe7820076592b3b9e2
Harold Peet <hpeet@microagebasics.com>
2018-05-07 11:20:59 -0400

    Getting Started

---
+++ aspnetcorebook/HaroldToDo/Services/ITodoItemService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HaroldToDo.Models;
+
+namespace HaroldToDo.Services
+{
+    public class ITodoItemService
+    {
+        Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync();
+    }
+}
---
+++ aspnetcorebook/ShaunToDoProject/Services/FakeTodoItemService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShaunToDoProject.Services
+{
+    public class FakeTodoItemService
+    {
+    }
+}
